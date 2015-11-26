51ab4c16db9b0926aef6c2493bf382e25711e0e7
aidizhang <aidizhang@college.harvard.edu>
2015-11-26 12:12:31 -0500

    exploring image processing libraries on pebbles.png

--- image_quilting.py
+++ image_quilting.py
@@ -6,7 +6,11 @@ import sys
 import os
 import png
 import itertools
+from pylab import *
+import matplotlib.pyplot as plt
 
+from PIL import Image
+from numpngw import write_png
 from image_quilting_helpers import verticalPathsCost
 from image_quilting_helpers import calculateCost
 
@@ -20,14 +24,104 @@ def patchDistance(refPatch, patches):
 	distances = np.sum(distances, axis=0)
 	return distances
 
+def mkPatches(img, patchSize):
+	'''
+	This function takes in an img with size img.shape and a patch size patchSize, returns an array of shape
+	(patchSize, patchSize, #num of patches), so (:,:,idx) returns the idx'th patch
+	'''
+	#note that img should have channel axis, so (x,y,channel)
+	width = img.shape[0] - patchSize
+	height = img.shape[1] - patchSize
+	nChannels = img.shape[2]
+	patches = np.zeros((patchSize, patchSize, nChannels, width*height), img.dtype)
+
+	#iterate through all patches from img and store in patches
+	k = 0
+	for i in width:
+		for j in height:
+			x1,x2 = i, i+patchSize
+			y1,y2 = j, j+patchSize
+			patches[:,:,:,k] = img[x:X,y:Y,:]
+			k += 1
+
+	return patches
+
+def getMatchingPatch(distances):
+	'''
+	Given a 1-D array of patch distances, choose matching patch index.
+	'''
+	d = distances
+	m = np.min(d)
+	#choose random index such that the distance is within 1.1x of minimum distance
+	threshold = 1.1*m
+	indices = np.where(d < threshold)[0]
+	idx = indices[np.random.randint(0,len(indices))]
+	return idx
+
+def mkTexture(textureSize, patches, overlap):
+	'''
+	Main function
+	'''
+	patchSize = patches.shape[0]
+	nChannels = patches.shape[2]
+	tileSize = patchSize - overlap
+	texture = np.zeros((textureSize[0], textureSize[1], nChannels), dtype=np.float32)
+
+	nPatches = patches.shape[2]
+
+	k = -1
+	width, height = int(math.ceil(textureSize[0]/float(tileSize))), int(math.ceil(textureSize[0]/float(tileSize)))
+	for i in range(width):
+		for j in range(height):
+			k += 1
+
+			#use random patch everytime
+			texture[0:patchSize, 0:patchSize, :] = patches[:,:,:,np.random.randint(0,nPatches)]
+
+	return texture
+
 if __name__ == "__main__":
+	img = Image.open("pebbles.png")
+	pixels = img.load()
+	#convert Image instance to numpy array for manipulation
+	img = np.array(img)
+	#print img.shape (88,100,3)
+	plt.imshow(img)
+	#img.save("pebbles_generate.png", "png")
+
 	#load image
-	reader = png.Reader("pebbles.png")
-	x, y, pixels, meta = reader.asDirect()
-	img = png.Image(pixels,meta)
-	print img
-	print img.shape
-	# img = np.vstack(itertools.imap(np.uint16, pngdata))
-	#make sure we have a grayscale/RGB image
-	# print img.shape, type(img)
+	# fname = "pebbles.png"
+	# reader = png.Reader(fname)
+	# w,h,pixels,metadata = reader.read_flat() #or reader.read(), asDirect()
+	# output = open("pebbles_new", "wb")
+	# writer = png.Writer(w,h,**metadata)
+	# writer.write_array(output, pixels)
+	# output.close()
+	# img = np.vstack(itertools.imap(np.uint8, pixels))
+	#print img.shape - shape (88,300)
+	# print img[20,:]
+	# write_png("pebbles_generate.png", img)
+
+
+	#patchSize = 30
+	# patches = mkPatches(img, 30)
+	
+	#textureSize = (300,150)
+	# img = mkTexture((300,150), patches, overlap=10)
+
+	#write out image to file
+	# outFname = os.path.splitext(fname)[0]+"_generated.png"
+	# write_png('pebbles_generate.png', img)
+
+
+
+
+
+
+
+
+
+
+
+
 
